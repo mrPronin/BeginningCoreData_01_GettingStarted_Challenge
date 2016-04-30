c1e6bba7288fec616f58f4dbfd31f73e3e71f801
Alexander Pronin <pronin.alx@gmail.com>
2016-04-30 12:20:05 +0300

    Initial Commit

---
+++ myDevices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myDevices
+//
+//  Created by Aleksandr Pronin on 4/30/16.
+//  Copyright © 2016 RIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
